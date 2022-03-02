e1756253fc5b7021da2ead4f6ccfa0ab2856fbf4
TheRealMG <56460873+TheRealMG@users.noreply.github.com>
2022-03-01 19:01:19 -0600

    Sidebar (Still WIP)

--- public_html/js/sidebar.js
+++ public_html/js/sidebar.js
@@ -8,11 +8,22 @@ function initializeMain() {
 function initializeCreate() {
     document.querySelector('.sidebar-left').innerHTML = "<form action=''>" +
     "<input type='text' id='title' name='title' placeholder='Title'><br>" +
+    "<label for='birthdaytime'>Start Time</label>" +
+    "<input type='datetime-local'>" +
+    "<label for='birthdaytime'>End Time</label>" +
+    "<input type='datetime-local'>" +
+    "<label class='allday'>All Day" +
+    "<input type='checkbox'>" +
+    "<span class='checkmark'></span>" +
+    "</label>" +
     "<input type='text' id='location' name='location' placeholder='Location'><br>" +
-    "<input type='text' id='description' name='description' placeholder='Description'><br>" +
+    "<textarea id='description' name='description' placeholder='Description'></textarea>" +
     "<input type='button' id='back' value='Back'>" +
     "<input type='submit' id='save' value='Save'>" +
     "</form>"
+    document.querySelector('#back').addEventListener('click', () => {
+        initializeMain();
+    })
 }
 
 initializeMain();

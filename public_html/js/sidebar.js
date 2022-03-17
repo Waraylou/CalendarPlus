dc67a5d2bd028ded87c17231a40403b7fbe80db3
TheRealMG <56460873+TheRealMG@users.noreply.github.com>
2022-03-17 13:39:06 -0500

    Optimized sidebar js file

--- public_html/js/sidebar.js
+++ public_html/js/sidebar.js
@@ -1,29 +1,119 @@
+const main = document.getElementById('sidebar');
+
+// Initializes the main sidebar view
 function initializeMain() {
-    document.querySelector('.sidebar-left').innerHTML = "<button type='button' id='create'>Create</button>";
-    document.querySelector('#create').addEventListener('click', () => {
+    const createButton = document.createElement('button');
+    createButton.type = 'button';
+    createButton.id = 'create';
+    createButton.innerText = 'Create';
+    createButton.addEventListener('click', () => {
+        clearSidebar();
         initializeCreate();
-    })
+    });
+    main.appendChild(createButton);
 }
 
+// CLears sidebar content
+function clearSidebar() {
+    main.innerHTML = '';
+}
+
+// Initializes the create sidebar
 function initializeCreate() {
-    document.querySelector('.sidebar-left').innerHTML = "<form id='manageEvent' action=''>" +
-    "<input type='text' id='title' name='title' placeholder='Title'><br>" +
-    "<label for='birthdaytime'>Start Time</label>" +
-    "<input type='datetime-local'>" +
-    "<label for='birthdaytime'>End Time</label>" +
-    "<input type='datetime-local'>" +
-    "<label class='allday'>All Day" +
-    "<input type='checkbox'>" +
-    "<span class='checkmark'></span>" +
-    "</label>" +
-    "<input type='text' id='location' name='location' placeholder='Location'><br>" +
-    "<textarea id='description' name='description' placeholder='Description'></textarea>" +
-    "</form>" + 
-    "<button type='button' id='back'>Back</button>" +
-    "<button id='save' form='manageEvent'>Save</button>"
-    document.querySelector('#back').addEventListener('click', () => {
-        initializeMain();
-    })
+    // Form creation
+    const createForm = document.createElement('form');
+    createForm.id = 'manageEvent';
+
+        // Container for form fields
+        const formFields = document.createElement('div');
+        formFields.className = 'formFields';
+
+            // Field for event name
+            const titleField = document.createElement('input');
+            titleField.type = 'text';
+            titleField.id = 'title';
+            titleField.name = 'title';
+            titleField.placeholder = 'Title';
+            formFields.appendChild(titleField);
+            
+            // Label for start time field
+            const startLabel = document.createElement('label');
+            startLabel.innerText = 'Start Time';
+            formFields.appendChild(startLabel);
+
+            // Field for start time
+            const startTime = document.createElement('input');
+            startTime.type = 'datetime-local';
+            formFields.appendChild(startTime);
+
+            // Label for end time field
+            const endLabel = document.createElement('label');
+            endLabel.innerText = 'End Time';
+            formFields.appendChild(endLabel);
+
+            // Field for end time
+            const endTime = document.createElement('input');
+            endTime.type = 'datetime-local';
+            formFields.appendChild(endTime);
+
+            // Label for all day checkbox
+            const allDayLabel = document.createElement('label');
+            allDayLabel.className = 'allday';
+            allDayLabel.innerText = 'All Day'
+
+                // Checkbox element
+                const allDay = document.createElement('input');
+                allDay.type = 'checkbox';
+                allDayLabel.appendChild(allDay);
+
+                // Element for custom checkbox (replaces default visually)
+                const checkmark = document.createElement('span');
+                checkmark.className = 'checkmark';
+                allDayLabel.appendChild(checkmark);
+
+            formFields.appendChild(allDayLabel);
+
+            // Field for event location
+            const locationField = document.createElement('input');
+            locationField.type = 'text';
+            locationField.id = 'location';
+            locationField.name = 'location';
+            locationField.placeholder = 'Location';
+            formFields.appendChild(locationField);
+
+            // Field for event description
+            const descriptionField = document.createElement('textarea');
+            descriptionField.id = 'description';
+            descriptionField.name = 'description';
+            descriptionField.placeholder = 'Description';
+            formFields.appendChild(descriptionField);
+
+        createForm.appendChild(formFields);
+        
+        // Container for form buttons
+        const formButtons = document.createElement('div');
+        formButtons.className = 'formButtons';
+
+            // Back button (returns user to main sidebar)
+            const backButton = document.createElement('button');
+            backButton.type = 'button';
+            backButton.id = 'back';
+            backButton.innerText = 'Back';
+            backButton.addEventListener('click', () => {
+                clearSidebar();
+                initializeMain();
+            });
+            formButtons.appendChild(backButton);
+
+            // Save button (saves event)
+            const saveButton = document.createElement('button');
+            saveButton.id = 'save';
+            saveButton.innerText = 'Save';
+            formButtons.appendChild(saveButton);
+
+        createForm.appendChild(formButtons);
+
+    main.appendChild(createForm);
 }
 
 /* When the user clicks on the button,
@@ -35,15 +125,15 @@ function myFunction() {
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
         }
-      }
     }
-  }
+}
 
 initializeMain();
